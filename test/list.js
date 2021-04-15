bf4bfb20db61e52d5e8a4939ee429332b081b0c1
Chris Manson <chris@manson.ie>
2021-04-15 09:46:00 +0100

    add test for list

---
+++ test/list.js
@@ -0,0 +1,42 @@
+import { expect } from 'chai';
+import { list } from '..';
+
+describe('list function', function () {
+  it('should output object with rules and files', function () {
+    const result = list(`${__dirname}/fixtures/list`);
+    expect(result).to.deep.equal({
+      'no-curly-component-invocation': [
+        'addon/templates/components/notification-container.hbs',
+        'addon/templates/components/notification-message.hbs',
+        'app/templates/application.hbs',
+        'app/templates/author.hbs',
+        'app/templates/error.hbs',
+        'app/templates/post.hbs',
+        'tests/dummy/app/templates/application.hbs',
+      ],
+      'no-action': [
+        'addon/templates/components/notification-message.hbs',
+        'tests/dummy/app/templates/application.hbs',
+      ],
+      'no-implicit-this': [
+        'addon/templates/components/notification-message.hbs',
+        'app/templates/application.hbs',
+        'app/templates/author.hbs',
+        'app/templates/error.hbs',
+        'app/templates/post.hbs',
+        'tests/dummy/app/templates/application.hbs',
+      ],
+      'no-invalid-interactive': ['addon/templates/components/notification-message.hbs'],
+      'no-triple-curlies': ['addon/templates/components/notification-message.hbs'],
+      'link-rel-noopener': [
+        'app/templates/application.hbs',
+        'app/templates/author.hbs',
+        'tests/dummy/app/templates/application.hbs',
+      ],
+      'no-log': ['app/templates/error.hbs'],
+      'no-unbalanced-curlies': ['tests/dummy/app/templates/application.hbs'],
+      'require-button-type': ['tests/dummy/app/templates/application.hbs'],
+      'require-input-label': ['tests/dummy/app/templates/application.hbs'],
+    });
+  });
+});
