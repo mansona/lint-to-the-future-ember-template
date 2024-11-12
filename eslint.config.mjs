854d3ed3ea76a88abc2b7577687c8abfa649813b
Chris Manson <chris.manson@mainmatter.com>
2024-11-12 15:14:39 +0000

    update eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  {
+    languageOptions: { globals: globals.node }
+  },
+  pluginJs.configs.recommended,
+  {
+    languageOptions: {
+
+      globals: globals.mocha,
+    },
+    files: ['test/*.mjs']
+  }
+];
