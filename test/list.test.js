cfface34ae149437775c461eb269da2c3abdd491
Peter Wagenet <peter@wagenet.us>
2025-03-07 10:50:01 -0800

    Fix test

--- test/list.test.js
+++ test/list.test.js
@@ -3,16 +3,16 @@ import { list } from '../main.mjs';
 import { expect, describe, it } from 'vitest';
 
 describe('list function', function () {
-  it('should output object with rules and files', function () {
-    const result = list('./test/fixtures/list');
+  it('should output object with rules and files', async function () {
+    const result = await list('./test/fixtures/list');
     expect(result).to.deep.equal({
       'no-curly-component-invocation': [
-        'addon/templates/components/notification-container.hbs',
-        'addon/templates/components/notification-message.hbs',
         'app/templates/application.hbs',
         'app/templates/author.hbs',
         'app/templates/error.hbs',
         'app/templates/post.hbs',
+        'addon/templates/components/notification-container.hbs',
+        'addon/templates/components/notification-message.hbs',
         'tests/dummy/app/templates/application.hbs',
       ],
       'no-action': [
@@ -20,11 +20,11 @@ describe('list function', function () {
         'tests/dummy/app/templates/application.hbs',
       ],
       'no-implicit-this': [
-        'addon/templates/components/notification-message.hbs',
         'app/templates/application.hbs',
         'app/templates/author.hbs',
         'app/templates/error.hbs',
         'app/templates/post.hbs',
+        'addon/templates/components/notification-message.hbs',
         'tests/dummy/app/templates/application.hbs',
       ],
       'no-invalid-interactive': ['addon/templates/components/notification-message.hbs'],
@@ -35,11 +35,11 @@ describe('list function', function () {
         'tests/dummy/app/templates/application.hbs',
       ],
       'no-log': [
-        'addon/templates/components/notification-thingy.hbs',
-        'addon/templates/components/notification-yoke.hbs',
         'app/templates/error.hbs',
         'app/templates/fancy.gjs',
         'app/templates/not-so-fancy.gts',
+        'addon/templates/components/notification-thingy.hbs',
+        'addon/templates/components/notification-yoke.hbs',
       ],
       'no-unbalanced-curlies': ['tests/dummy/app/templates/application.hbs'],
       'require-button-type': ['tests/dummy/app/templates/application.hbs'],
